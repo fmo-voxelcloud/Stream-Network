aa90f83c7e7e160ce355a3b617fb04e8366ab5de
fan.mo <fmo@voxelcloud.net.cn>
2021-05-14 18:29:42 +0800

    init

---
+++ libs/nets/stream_net.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# author: fan.mo
+# email: fmo@voxelcloud.net.cn
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class StreamNet(nn.Module):
+    arg1 = 0.2
+    arg2 = 0.1
+
+    def __init__(self, inp_dim, out_dim, depth=2, width=512, activation="relu"):
+        super(StreamNet, self).__init__()
+
+        self.inp_dim = inp_dim
+        self.out_dim = out_dim
+        self.depth = depth
+        self.width = width
+        self.activation = activation
+
+        self.layers = self.make_layers()
+
+
+    def make_layers(self):
+        if self.activation == "relu":
+            act = nn.ReLU()
+        elif self.activation == "leaky_relu":
+            act = nn.LeakyReLU(self.arg1)
+        else:
+            raise ValueError("activation method must be in ['relu', 'leaky_relu']")
+
+        layers = list()
+        layers.append(nn.Linear(self.inp_dim, self.width))
+        layers.append(act)
+
+        for _ in range(self.depth-1):
+            layers.append(nn.Linear(self.width, self.width))
+            layers.append(act)
+
+        layers.append(nn.Linear(self.width, self.out_dim))
+        layers.append(nn.Softmax(dim=1))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+if __name__ == "__main__":
+
+    net = StreamNet(50, 10)
+    print(net)
+    x = torch.randn(8, 50)
+    y = net(x)
+    print(y[0])
+
---
+++ train.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# author: fan.mo
+# email: fmo@voxelcloud.net.cn
+
+import os
+import json
+import timm
+
+from tqdm import tqdm
+from libs.nets.efficientnet_pytorch import EfficientNet
+from libs.nets.stream_net import StreamNet
+
+
+def hook(module, inp, outp):
+    return outp.clone().detach()
+
+
+def train(config):
+
+    merge_method = config["merge_method"]
+    training_stage = config["training_stage"]
+
+    model_list = list()
+
+    for idx, model_config in enumerate(config["models"]):
+
+        stream = dict()
+
+        # load model architect
+        model_name = model_config["model_name"]
+        num_classes = model_config["num_classes"]
+        if model_name.startswith("efficientnet-b"):
+            model = EfficientNet.from_pretrained(model_name,
+                num_classes=num_classes)
+        else:
+            model = timm.create_model(model_name, num_classes=num_classes, pretrained=True)
+
+        # load trained model
+        ckpt_path = model_config['checkpoint_path']
+        state_dict = model_config['state_dict']
+        # model.load_state_dict(torch.load(ckpt_path)[state_dict])
+
+        # add hook to extract feature(s)
+        feature_name = model_config['stream_feature_names']
+        handles = list()
+        for fn in feature_name:
+            ftr = eval(f"model.{fn}.register_forward_hook(hook)")
+            handles.append(ftr)
+            print(ftr)
+
+        # transform for image preprocess
+        image_size = model_config['image_size']
+        resize_mode = model_config['resize_mode']
+        # TODO
+
+        # dataloader
+        data_config = config['data_loader'][idx]
+        # TODO
+
+        stream['model'] = model
+        stream['hook'] = handles
+        stream['dataloader'] = data_loader
+
+        model_list.append(stream)
+
+    sw_config = config['stream_weights']
+    if sw_config['pretrained_model']:
+        sw = torch.load(sw_conig['pretrained_model'])
+    else:
+        pass
+        # sw = # TODO
+
+
+if __name__ == "__main__":
+
+    config = json.load(open('configs/exp_train.cfg'))
+    train(config)
